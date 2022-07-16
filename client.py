9533c4e9b9ab5c0d001afc9b2c80dfd916693a85
Shayan_Jahan <shayantec95@gmail.com>
2022-07-16 05:33:15 +0430

    Change client code

--- client.py
+++ client.py
@@ -33,11 +33,14 @@ class Client:
                 command = " ".join(command_parts)
 
             if any([command.split()[0] == 'mkdir', command.split()[0] == 'touch', command.split()[0] == 'cd',
-                    command.split()[0] == 'read']) and len(command.split()) == 2:
+                    command.split()[0] == 'read', command.split()[0] == 'ls']) and len(command.split()) == 2:
                 command_parts = command.split()
                 new_directory_parts = []
-                for part in command_parts[1].split('/'):
-                    new_directory_parts.append(self.encode(part))
+                for part in command_parts[-1].split('/'):
+                    if part == '..' or part == '.':
+                        new_directory_parts.append(part)
+                    else:
+                        new_directory_parts.append(self.encode(part))
 
                 command_parts[1] = "/".join(new_directory_parts)
                 command = " ".join(command_parts)
@@ -46,8 +49,12 @@ class Client:
                     (len(command.split()) == 2 or len(command.split()) == 3):
                 command_parts = command.split()
                 new_directory_parts = []
+
                 for part in command_parts[-1].split('/'):
-                    new_directory_parts.append(self.encode(part))
+                    if part == '..' or part == '.':
+                        new_directory_parts.append(part)
+                    else:
+                        new_directory_parts.append(self.encode(part))
 
                 command_parts[-1] = "/".join(new_directory_parts)
                 command = " ".join(command_parts)
@@ -62,10 +69,9 @@ class Client:
 
                 text = " ".join(command_parts[1:])
                 encoded_text = self.encode(text)
-                command = 'edit ' + command_parts[1] + ' ' + encoded_text\
+                command = 'edit ' + command_parts[1] + ' ' + encoded_text
 
             self.server.get_command(command)
-
 
     def sum(self, a, b):
         result = ""
