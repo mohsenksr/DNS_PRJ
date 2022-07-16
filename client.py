4f9bfa76b9415fe01621299824fdcdba40d6162c
mohsenksr <mohsen.kassiri@live.com>
2022-07-16 05:03:21 +0430

    add client sending messages encode

--- client.py
+++ client.py
@@ -35,6 +35,38 @@ class Client:
                 command_parts.append(self.encode(command_parts[1]))
                 command = " ".join(command_parts)
 
+            if any([command.split()[0] == 'mkdir', command.split()[0] == 'touch', command.split()[0] == 'cd',
+                    command.split()[0] == 'read']) and len(command.split()) == 2:
+                command_parts = command.split()
+                new_directory_parts = []
+                for part in command_parts[1].split('/'):
+                    new_directory_parts.append(self.encode(part))
+
+                command_parts[1] = "/".join(new_directory_parts)
+                command = " ".join(command_parts)
+
+            if any([command.split()[0] == 'rm', command.split()[0] == 'mv']) and \
+                    (len(command.split()) == 2 or len(command.split()) == 3):
+                command_parts = command.split()
+                new_directory_parts = []
+                for part in command_parts[-1].split('/'):
+                    new_directory_parts.append(self.encode(part))
+
+                command_parts[-1] = "/".join(new_directory_parts)
+                command = " ".join(command_parts)
+
+            if command.split()[0] == 'edit' and len(command.split()) > 2:
+                command_parts = command.split()
+                new_directory_parts = []
+                for part in command_parts[1].split('/'):
+                    new_directory_parts.append(self.encode(part))
+
+                command_parts[1] = "/".join(new_directory_parts)
+
+                text = " ".join(command_parts[1:])
+                encoded_text = self.encode(text)
+                command = 'edit ' + command_parts[1] + ' ' + encoded_text
+
             self.server.get_command(command)
 
     def encode(self, text):
